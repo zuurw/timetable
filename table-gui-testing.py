8f280a0f68c0fe8655e33d1209f6ed6b17f594ab
Cliffside-Dev <jamesmblount@outlook.com>
2018-06-18 22:58:37 +0100

    added commentation

--- table-gui-testing.py
+++ table-gui-testing.py
@@ -1,14 +1,19 @@
+#import relevant modules
 from tkinter import *
+import json
 
+#define Tk and the window, title and favicon.
 master = Tk()
 master.geometry('300x300')
 master.title("tableTimes")
 master.iconbitmap("favicon.ico")
 
+#defines code for the 'add subject button'
 def addsubject():
     
-
+#content / buttons and GUI
 AddSubjectB = Button(master, text="Add new subject", command=addsubject)
 AddSubjectB.pack(expand=YES)
 
+#loops and starts the GUI
 master.mainloop()
